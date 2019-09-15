b2c0f707ecc0bf1a569e03e6c8bf5f20b71c89c1
Naume-k <kobnaome@gmail.com>
2019-09-15 17:06:18 +0200

    created the file config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+# import os
+# class Config:
+#     '''
+#     General configuration parent class
+#     '''
+#     SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+#     ARTICLES_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+
+#     SOURCES_API_KEY = ('f80f23ee3b83401e951859c48cf71821')
+#     print(SOURCES_API_KEY)
+#     # NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+
+# class ProdConfig(Config):
+#     '''
+#     Production  configuration child class
+
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     pass
+
+
+# class DevConfig(Config):
+#     '''
+#     Development  configuration child class
+
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+
+#     DEBUG = True
+    
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig
+# }
