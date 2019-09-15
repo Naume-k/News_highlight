8b5952265277f34b7f83b460ba881d616eb34cf1
Naume-k <kobnaome@gmail.com>
2019-09-15 17:22:02 +0200

    created folder tests and test files needed

---
+++ tests/test_sources.py
@@ -0,0 +1,19 @@
+# import unittest
+# from app.models import Sources
+
+# class SourcesTest(unittest.TestCase):
+#     '''
+#     Test Class to test the behaviour of the news class
+#     '''
+
+#     def setUp(self):
+#         '''
+#         Set up method that will run before every Test
+#         '''
+#         self.new_sources = Sources(1234,'A thrilling new Python Series','/khsjha27hbs',8.5,129993,'ffffffffffff','date')
+
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.new_sources,Sources))
+   
+# if __name__ == '__main__':
+#     unittest.main()     
