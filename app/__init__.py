a566fc452c8627bec0c4af51460051b009a160f8
Naume-k <kobnaome@gmail.com>
2019-09-15 17:49:59 +0200

    initialised the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
-# def create_app(config_name):
+def create_app(config_name):
 
-#     app = Flask(__name__)
+    app = Flask(__name__)
 
 #     # Creating the app configurations
 #     app.config.from_object(config_options[config_name])
