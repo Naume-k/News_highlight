489d47fff71e165c5b236278b41e3b97d81dd7f7
Naume-k <kobnaome@gmail.com>
2019-09-15 17:45:42 +0200

    created the models.py file

---
+++ app/models.py
@@ -0,0 +1,31 @@
+# class Sources:
+#     '''
+#     news class to define news Objects
+#     '''
+
+    
+#     def __init__(self,id,name,description,url,category,country):
+#         self.id =id
+#         self.name = name
+#         self.description = description
+#         self.url = url 
+#         self.category = category
+#         self.country = country
+
+
+
+# class Article:
+
+#     '''
+#     Article class that defines the article objects
+#     '''
+
+#     def __init__(self,id,tittle,author,description,url,urlToImage,publishedAt):
+#         self.id = id
+#         self.tittle = tittle
+#         self.author = author
+#         self.description = description
+#         self.url = url
+#         self.urlToImage = urlToImage
+#         self.publishedAt = publishedAt
+
