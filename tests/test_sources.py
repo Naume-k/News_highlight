140da5f8a6cc62d6d7c8f7e7fedd80ae5ec30655
Naume-k <kobnaome@gmail.com>
2019-09-15 17:23:48 +0200

    imported unittest and put the tests

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,19 +1,19 @@
-# import unittest
-# from app.models import Sources
+import unittest
+from app.models import Sources
 
-# class SourcesTest(unittest.TestCase):
-#     '''
-#     Test Class to test the behaviour of the news class
-#     '''
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the news class
+    '''
 
-#     def setUp(self):
-#         '''
-#         Set up method that will run before every Test
-#         '''
-#         self.new_sources = Sources(1234,'A thrilling new Python Series','/khsjha27hbs',8.5,129993,'ffffffffffff','date')
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_sources = Sources(1234,'A thrilling new Python Series','/khsjha27hbs',8.5,129993,'ffffffffffff','date')
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_sources,Sources))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_sources,Sources))
    
-# if __name__ == '__main__':
-#     unittest.main()     
+if __name__ == '__main__':
+    unittest.main()     
