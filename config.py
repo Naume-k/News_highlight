dd5f16a7f840a7bb856f5c697a8ef2d49e121220
Naume-k <kobnaome@gmail.com>
2019-09-15 17:09:18 +0200

    added a subclass of devconfig and made the debug true

--- config.py
+++ config.py
@@ -22,15 +22,15 @@ class ProdConfig(Config):
     pass
 
 
-# class DevConfig(Config):
-#     '''
-#     Development  configuration child class
+class DevConfig(Config):
+    '''
+    Development  configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
 
-#     DEBUG = True
+    DEBUG = True
     
 # config_options = {
 # 'development':DevConfig,
