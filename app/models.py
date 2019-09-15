48a592f8ca905ba1aaf2e71292bc9abcd642beeb
Naume-k <kobnaome@gmail.com>
2019-09-15 17:47:11 +0200

    created a class sources and initialised the arguments

--- app/models.py
+++ app/models.py
@@ -1,16 +1,16 @@
-# class Sources:
-#     '''
-#     news class to define news Objects
-#     '''
+class Sources:
+    '''
+    news class to define news Objects
+    '''
 
     
-#     def __init__(self,id,name,description,url,category,country):
-#         self.id =id
-#         self.name = name
-#         self.description = description
-#         self.url = url 
-#         self.category = category
-#         self.country = country
+    def __init__(self,id,name,description,url,category,country):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url 
+        self.category = category
+        self.country = country
 
 
 
